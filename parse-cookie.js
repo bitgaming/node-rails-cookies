4c07630331cfde093675d6e0bec43bdd137f3ec4
Ashvin <ashvin@bitgamelabs.com>
2016-11-16 10:40:28 +0800

    Change code style

--- parse-cookie.js
+++ parse-cookie.js
@@ -2,7 +2,7 @@ var crypto = require('crypto');
 
 // constant time comparison
 function compare(a, b) {
-  if ( a.length !== b.length ) {
+  if (a.length !== b.length) {
     return false;
   }
 
@@ -15,32 +15,28 @@ function compare(a, b) {
 }
 
 module.exports = function(options) {
-  var secret = crypto.pbkdf2Sync(options.base, options.salt, options.iterations, options.keylen / 2, 'sha1')
-    , signed_secret = crypto.pbkdf2Sync(options.base, options.signed_salt, options.iterations, options.keylen, 'sha1')
-    ;
+  var secret = crypto.pbkdf2Sync(options.base, options.salt, options.iterations, options.keylen / 2, 'sha1');
+  var signed_secret = crypto.pbkdf2Sync(options.base, options.signed_salt, options.iterations, options.keylen, 'sha1');
 
   return function(cookie, cipherName) {
-
-    var signed_parts = cookie.split('--')
-      , hmac = crypto.createHmac('sha1', signed_secret)
-      , digest
-      ;
+    var signed_parts = cookie.split('--');
+    var hmac = crypto.createHmac('sha1', signed_secret);
 
     hmac.update(signed_parts[0]);
-    digest = hmac.digest('hex');
+    var digest = hmac.digest('hex');
 
-    if ( !compare(signed_parts[1], digest) ) return console.log('not valid');
+    if (!compare(signed_parts[1], digest)) {
+      return console.log('not valid');
+    }
 
-    var message = (new Buffer(signed_parts[0], 'base64').toString())
-      , parts = message.split('--').map(function(part) {
-          return new Buffer(part, 'base64');
-        })
-      ;
+    var message = new Buffer(signed_parts[0], 'base64').toString();
+    var parts = message.split('--').map(function(part) {
+      return new Buffer(part, 'base64');
+    });
 
-    var cipher = crypto.createDecipheriv(cipherName, secret, parts[1])
-      , part = new Buffer(cipher.update(parts[0])).toString('utf8')
-      , final = cipher.final('utf8')
-      ;
+    var cipher = crypto.createDecipheriv(cipherName, secret, parts[1]);
+    var part = new Buffer(cipher.update(parts[0])).toString('utf8');
+    var final = cipher.final('utf8');
 
     return [part, final].join('');
   }
